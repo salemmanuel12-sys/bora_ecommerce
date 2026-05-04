85be55cef672b3406c08e1c0b53c6eb9fbe85ce5
salemmanuel12-sys <puntocrecesoft@gmail.com>
2026-05-03 22:46:17 -0600

    cambio en de para leer env version 16

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,4 +1,4 @@
-const { sequelize } = require('../config/db');
+const { sequelize } = require('../config/db').sequelize;
 const setupPermisosAssociations = require('./permiso.associations');
 
 const Administrador = require('./Administrador')(sequelize);
