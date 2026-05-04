a4035ac6516e8653d264b0ef11804eff02d49bf6
salemmanuel12-sys <puntocrecesoft@gmail.com>
2026-05-04 10:22:13 -0600

    cambio en los models/loader6

--- backend/src/models/db.js
+++
@@ -1,7 +0,0 @@
-const { sequelize } = require('../config/db');
-const initModels = require('./index');
-
-const db = initModels(sequelize);
-console.log('🔥 MODELOS INICIALIZADOS UNA SOLA VEZ');
-
-module.exports = db;
