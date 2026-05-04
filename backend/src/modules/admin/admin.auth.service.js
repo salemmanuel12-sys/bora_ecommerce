320f94a1c703f819b5bdd4ae5eab63258a3383d7
salemmanuel12-sys <puntocrecesoft@gmail.com>
2026-05-03 21:42:53 -0600

    cambio en de para leer env version 13

--- backend/src/modules/admin/admin.auth.service.js
+++ backend/src/modules/admin/admin.auth.service.js
@@ -1,14 +1,9 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
-const setupModels = require('../../models');
-const { sequelize } = require('../../config/db');
+const { Op } = require('sequelize');
 const HttpError = require('../../utils/httpError');
-const {
-  Administrador,
-  AdminRefreshToken,
-  AdminResetCode
-} = setupModels(sequelize);
+const { Administrador, AdminRefreshToken, AdminResetCode } = require('../../models');
 const { sendResetCode } = require('../../utils/email');
 const emailService = require('../../services/emailService');
 const {
